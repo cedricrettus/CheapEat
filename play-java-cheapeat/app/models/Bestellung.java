6ecfdd9fd58193a7b12d12819c65c96194455892
[cedric sutter] <[cedric.rettus@gmail.com]>
2017-10-16 19:22:03 +0200

    New Table

--- play-java-cheapeat/app/models/Bestellung.java
+++ play-java-cheapeat/app/models/Bestellung.java
@@ -1,4 +1,35 @@
 package models;
 
+import javax.persistence.*;
+import javax.xml.crypto.Data;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "bestellung")
 public class Bestellung {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public int id;
+
+    public int benutzer_id;
+
+    public int angebot_id;
+
+    public int menge;
+
+    public int prozesscode;
+
+    @Override
+    public String toString() {
+        return "Bestellung{" +
+                "id=" + id +
+                ", benutzer_id=" + benutzer_id +
+                ", angebot_id=" + angebot_id +
+                ", menge=" + menge +
+                ", prozesscode=" + prozesscode +
+                '}';
+    }
 }
