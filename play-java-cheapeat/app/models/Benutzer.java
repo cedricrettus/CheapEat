33973392fe269efc548384c0b3a7dbb6128d3991
[cedric sutter] <[cedric.rettus@gmail.com]>
2017-10-17 10:21:02 +0200

    New Table Benutzer

--- play-java-cheapeat/app/models/Benutzer.java
+++ play-java-cheapeat/app/models/Benutzer.java
@@ -1,4 +1,38 @@
 package models;
 
+import javax.persistence.*;
+import javax.xml.crypto.Data;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "benutzer")
 public class Benutzer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public int id;
+
+    public String name;
+
+    public String nachname;
+
+    public String benutzername;
+
+    public String email;
+
+    public int bewertung;
+
+    @Override
+    public String toString() {
+        return "Benutzer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", nachname='" + nachname + '\'' +
+                ", benutzername='" + benutzername + '\'' +
+                ", email='" + email + '\'' +
+                ", bewertung=" + bewertung +
+                '}';
+    }
 }
