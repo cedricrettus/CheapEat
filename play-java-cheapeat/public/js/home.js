1a972871bf6151665e054cbc8bef65b1544362f8
[cedric sutter] <[cedric.rettus@gmail.com]>
2017-10-20 13:16:33 +0200

    homeseite bearbeitet

--- play-java-cheapeat/public/js/home.js
+++ play-java-cheapeat/public/js/home.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     /*$.get('/angebote', function(angebote){
         $.each(angebote, function(index, angebot){
             $('#angebote').append(
+
                 $('<li>').append(angebot.titel));
         })
     })*/
